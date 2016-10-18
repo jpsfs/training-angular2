9a01dfb247e710eac374307164820a8508a83052
José Pedro Silva <jose.silva@criticalmanufacturing.com>
2016-10-18 14:27:23 +0100

    Changes to demonstrate debug!

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+        <h1>My First Angular App</h1>
+        <button (click)="onClick($event)">Click Me!</button>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+
+    constructor(){
+        console.log("App Component was constructed!");
+    }
+
+    public onClick(event: Event): void{
+        console.log("Button Clicked!");
+    }
+}
